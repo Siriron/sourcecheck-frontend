43d48d00a66563a9197cae8053e21162226bb9dd
sirironheart <127705750+Siriron@users.noreply.github.com>
2026-06-25 07:47:32 +0600

    Add files via upload

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,24 +1,15 @@
 import { createClient } from 'genlayer-js'
-import { studionet } from 'genlayer-js/chains'
+import { studionet, testnetBradbury } from 'genlayer-js/chains'
 import { type NetworkKey } from './chains'
 
-// Bradbury manual config since it may not be in genlayer-js/chains yet
-const bradburyChain = {
-  id: 4221,
-  name: 'Bradbury',
-  rpcUrls: { default: { http: ['https://rpc-bradbury.genlayer.com'] } },
-} as any
-
-const studionetChain = studionet as any
-
 export function getReadClient(network: NetworkKey) {
-  const chain = network === 'bradbury' ? bradburyChain : studionetChain
+  const chain = network === 'bradbury' ? testnetBradbury : studionet
   return createClient({ chain })
 }
 
 export async function getWriteClient(network: NetworkKey) {
   if (!window.ethereum) throw new Error('No wallet detected. Please install MetaMask.')
-  const chain = network === 'bradbury' ? bradburyChain : studionetChain
+  const chain = network === 'bradbury' ? testnetBradbury : studionet
   const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
   const client = createClient({
     chain,
